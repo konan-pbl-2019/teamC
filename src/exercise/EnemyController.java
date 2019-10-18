e6526278fc85a61c29ffeef83cc84817a99b66ce
s1871044 <s1871044@d-13102-064.ku-kinds.konan-u.ac.jp>
2019-10-18 17:28:41 +0900

    敵AIの追加

--- src/exercise/EnemyController.java
+++ src/exercise/EnemyController.java
@@ -3,53 +3,39 @@ package exercise;
 public class EnemyController implements IController {
 	Vector2 center = new Vector2(0, 0);
 	public void Run(BasePeople people) {
+		double moveValue = 0;
 		if(people.GetImage().getPosition().getY() > center.y) {
 			if(people.GetImage().getPosition().getX() < center.x) {
 				people.GetImage().moveRight(1);
-				System.out.println(people.GetImage().getPosition().getX());	
+				if(Math.abs(people.GetImage().getPosition().getX())>=0.05) {
+					moveValue = (-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX();
+					people.GetImage().moveDown((-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX());
+				}
 			}
 			if(people.GetImage().getPosition().getX()>center.x) {
 				people.GetImage().moveLeft(1);
-			}
-			if(people.GetImage().getPosition().getX()>center.x) {
-				if(people.GetImage().getPosition().getY()>center.y) {
-					people.GetImage().moveDown(people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX());
-				}
-				if(people.GetImage().getPosition().getY()<center.y) {
-					people.GetImage().moveUp(people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX());
-				}
-			}else {
-				if(people.GetImage().getPosition().getY()>center.y) {
-					people.GetImage().moveDown(people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX()*(-1));
-				}
-				if(people.GetImage().getPosition().getY()<center.y) {
-					people.GetImage().moveUp(people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX()*(-1));
+				if(Math.abs(people.GetImage().getPosition().getX())>=0.05) {
+					moveValue = (-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX()*(-1);
+					people.GetImage().moveDown((-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX()*(-1));
 				}
 			}
 		}else {
 			if(people.GetImage().getPosition().getX() < center.x) {
 				people.GetImage().moveRight(1);
-				System.out.println(people.GetImage().getPosition().getX());	
+				if(Math.abs(people.GetImage().getPosition().getX())>=0.05) {
+					moveValue = (-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX();
+					people.GetImage().moveDown((-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX());
+				}
 			}
 			if(people.GetImage().getPosition().getX()>center.x) {
 				people.GetImage().moveLeft(1);
-			}
-			if(people.GetImage().getPosition().getX()>center.x) {
-				if(people.GetImage().getPosition().getY()>center.y) {
-					people.GetImage().moveDown((-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX());
-				}
-				if(people.GetImage().getPosition().getY()<center.y) {
-					people.GetImage().moveUp((-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX());
-				}
-			}else {
-				if(people.GetImage().getPosition().getY()>center.y) {
-					people.GetImage().moveDown((-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX()*(-1));
-				}
-				if(people.GetImage().getPosition().getY()<center.y) {
-					people.GetImage().moveUp((-1)*people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX()*(-1));
+				if(Math.abs(people.GetImage().getPosition().getX())>=0.05) {
+					moveValue = people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX()*(-1);
+					people.GetImage().moveUp(people.GetImage().getPosition().getY()/people.GetImage().getPosition().getX()*(-1));
 				}
 			}
 		}
+		System.out.println("moveValue = " + moveValue);
 	}
 
 }
